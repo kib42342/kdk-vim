c487006b0d9057d8d3fd1d3e049a3ba041265086
慈濟大學電算中心陳信龍 <kib42342@mail.tcu.edu.tw>
2017-03-14 16:32:48 +0800

    add apt install

--- new-install.sh
+++ new-install.sh
@@ -1,7 +1,8 @@
 git submodule init
 git submodule update
 sudo apt update
-sudo apt install sdcv
+sudo apt install exuberant-ctags -y
+sudo apt install sdcv -y
 cd .. && ln -s kdk-vim .vim -f
 ln -s kdk-vim/vimrc-kdk .vimrc -f 
 ln -s kdk-vim/bashrc .bashrc -f 
