05db9490e1e6f96d40025976175f5c44a36f21f8
慈濟大學電算中心陳信龍 <kib42342@mail.tcu.edu.tw>
2017-03-14 16:27:14 +0800

    add sdcv dictionary

--- new-install.sh
+++ new-install.sh
@@ -2,3 +2,8 @@ git submodule init
 git submodule update
 sudo apt update
 sudo apt install sdcv
+cd .. && ln -s kdk-vim .vim -f
+ln -s kdk-vim/vimrc-kdk .vimrc -f 
+ln -s kdk-vim/bashrc .bashrc -f 
+ln -s kdk-vim/stardict .stardict -f
+cd -
