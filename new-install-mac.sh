18a9a90a91fbca50b71009dc2b37d3386bd29eef
kib42342 <kib42342@mail.tcu.edu.tw>
2017-03-14 20:00:35 +0800

    add macos vim config

---
+++ new-install-mac.sh
@@ -0,0 +1,9 @@
+git submodule init
+git submodule update
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+brew install sdcv 
+cd .. && ln -sF kdk-vim .vim 
+ln -sf kdk-vim/vimrc-kdk .vimrc  
+ln -sf kdk-vim/bashrc-mac .bashrc 
+ln -sF kdk-vim/stardict .stardict 
+cd -
