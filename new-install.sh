cd37aeca155ce4329421f45eb4ef6e2330efcb92
慈濟大學電算中心陳信龍 <kib42342@mail.tcu.edu.tw>
2017-03-15 09:41:54 +0800

    change bashrc,new-install.sh

--- new-install.sh
+++ new-install.sh
@@ -1,10 +1,14 @@
 git submodule init
 git submodule update
 sudo apt update
+sudo apt install vim-nox-py2 -y
 sudo apt install exuberant-ctags -y
 sudo apt install sdcv -y
-cd .. && ln -s kdk-vim .vim -f
-ln -s kdk-vim/vimrc-kdk .vimrc -f 
-ln -s kdk-vim/bashrc .bashrc -f 
-ln -s kdk-vim/stardict .stardict -f
+sudo apt install python-pip 
+sudo apt install powerline 
+sudo apt install fonts-powerline 
+cd .. && ln -sF kdk-vim .vim 
+ln -sf kdk-vim/vimrc-kdk .vimrc 
+ln -sf kdk-vim/bashrc .bashrc 
+ln -sF kdk-vim/stardict .stardict 
 cd -
