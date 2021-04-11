7846732ed7420108daa31814c85275fb9a05d3b1
francis <kib42342@gmail.com>
2021-04-11 08:59:13 +0800

    位於分支 master  您的分支與上游分支 'origin/master' 一致。
     要提交的變更：
    	修改：     bundle/ctrlp
    	修改：     bundle/delimitMate
    	修改：     bundle/emmet-vim
    	修改：     bundle/neocomplcache
    	修改：     bundle/nerdcommenter
    	修改：     bundle/nerdtree
    	修改：     bundle/phpcomplete
    	修改：     bundle/supertab
    	修改：     bundle/unite
    	修改：     bundle/vim-airline
    	修改：     bundle/vim-airline-themes
    	修改：     bundle/vim-coffeescript
    	修改：     bundle/vim-css-color
    	修改：     bundle/vim-easymotion
    	修改：     bundle/vim-fugitive
    	修改：     bundle/vim-javascript
    	修改：     bundle/vim-markdown
    	修改：     bundle/vim-pathogen
    	修改：     bundle/vim-ragtag
    	修改：     bundle/vim-rails
    	修改：     bundle/vim-repeat
    	修改：     bundle/vim-ruby
    	修改：     bundle/vim-sass
    	修改：     bundle/vim-surround
    	修改：     new-install.sh
    
     尚未暫存以備提交的變更：
    	修改：     bundle/Align (未追蹤的內容)
    	修改：     bundle/cocoa.vim (未追蹤的內容)
    	修改：     bundle/ctrlp (修改的內容)
    	修改：     bundle/delimitMate (修改的內容)
    	修改：     bundle/emmet-vim (修改的內容)
    	修改：     bundle/haskell.vim (修改的內容)
    	修改：     bundle/matchit (修改的內容)
    	修改：     bundle/neocomplcache (修改的內容)
    	修改：     bundle/nerdcommenter (修改的內容)
    	修改：     bundle/nerdtree (修改的內容)
    	修改：     bundle/nginx (修改的內容)
    	修改：     bundle/phpcomplete (修改的內容)
    	修改：     bundle/scss-syntax (修改的內容)
    	修改：     bundle/snipmate (修改的內容)
    	修改：     bundle/supertab (修改的內容)
    	修改：     bundle/taglist.vim (修改的內容)
    	修改：     bundle/tlib_vim (修改的內容)
    	修改：     bundle/unite (修改的內容)
    	修改：     bundle/vim-addon-mw-utils (修改的內容)
    	修改：     bundle/vim-airline (修改的內容)
    	修改：     bundle/vim-airline-themes (修改的內容)
    	修改：     bundle/vim-coffeescript (修改的內容)
    	修改：     bundle/vim-css-color (修改的內容)
    	修改：     bundle/vim-easymotion (修改的內容)
    	修改：     bundle/vim-fugitive (修改的內容)
    	修改：     bundle/vim-indent-object (修改的內容)
    	修改：     bundle/vim-jade (修改的內容)
    	修改：     bundle/vim-javascript (修改的內容)
    	修改：     bundle/vim-jst (修改的內容)
    	修改：     bundle/vim-markdown (修改的內容)
    	修改：     bundle/vim-nerdtree-tabs (修改的內容)
    	修改：     bundle/vim-octopress (修改的內容)
    	修改：     bundle/vim-pathogen (修改的內容)
    	修改：     bundle/vim-powerline (修改的內容)
    	修改：     bundle/vim-ragtag (修改的內容)
    	修改：     bundle/vim-rails (修改的內容)
    	修改：     bundle/vim-repeat (修改的內容)
    	修改：     bundle/vim-ruby (修改的內容)
    	修改：     bundle/vim-ruby-minitest (修改的內容)
    	修改：     bundle/vim-sass (修改的內容)
    	修改：     bundle/vim-surround (修改的內容)
    	修改：     dictionary (修改的內容)

--- new-install.sh
+++ new-install.sh
@@ -7,8 +7,11 @@ sudo apt install sdcv -y
 sudo apt install python-pip 
 sudo apt install powerline 
 sudo apt install fonts-powerline 
+sudo apt install fzf
 cd .. && ln -sF kdk-vim .vim 
 ln -sf kdk-vim/vimrc-kdk .vimrc 
 ln -sf kdk-vim/bashrc .bashrc 
 ln -sF kdk-vim/stardict .stardict 
 cd -
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
